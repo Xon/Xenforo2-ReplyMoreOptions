ffc57453854dad2719857894b3a8a344c674e82f
Xon <635541+Xon@users.noreply.github.com>
2025-08-26 01:12:35 +0800

    XF2.3 export

--- upload/src/addons/SV/ReplyMoreOptions/_output/extension_hint.php
+++ upload/src/addons/SV/ReplyMoreOptions/_output/extension_hint.php
@@ -3,6 +3,11 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
+/**
+ * @noinspection PhpMultipleClassesDeclarationsInOneFile
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
 namespace SV\ReplyMoreOptions\XF\Pub\Controller
 {
 	class XFCP_Conversation extends \XF\Pub\Controller\Conversation {}
